c13d19f717eb7984f7a23f94ca07dc878a8bacc9
Eric Satterwhite <esatterwhite@wi.rr.com>
2016-09-04 15:55:25 -0500

    introduce frame times for main game loop

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ extern crate sdl2;
 
 mod phi;
 mod views;
-use sdl2::pixels::Color;
-use phi::Events;
+
+use phi::{Events, Phi, View, ViewAction};
 
 
 fn main() {
@@ -11,28 +11,52 @@ fn main() {
     // initialize SDL2
     let sdl_context =  sdl2::init().unwrap();
     let video = sdl_context.video().unwrap();
+    let mut timer = sdl_context.timer().unwrap();
 
     // create a new window
     let window = video.window("Arcade Shooter", 800, 600)
             .position_centered().opengl()
             .build().unwrap();
 
-    let mut renderer = window.renderer()
-            .accelerated()
-            .build()
-            .unwrap();
-    
+    let mut context = Phi{
+        events: Events::new( sdl_context.event_pump().unwrap() ),
+        renderer: window.renderer().accelerated().build().unwrap(),
+    };
 
-    let mut events = Events::new( sdl_context.event_pump().unwrap() );
+    let mut current_view: Box<View> = Box::new(self::views::DefaultView);
+
+    // Frame timing
+
+    let interval = 1_000 / 60;
+    let mut before = timer.ticks();
+    let mut last_second = timer.ticks();
+    let mut fps = 0u16;
+
 
     loop {
-        events.pump();
-        if events.now.quit || events.now.key_escape == Some(true) {
-            break
+        let now = timer.ticks();
+        let delta = now - before;
+        let elapsed = delta as f64 / 1_000.0;
+
+
+        if delta < interval {
+            timer.delay( interval - delta );
+                continue;
         }
-        // render a black window
-        renderer.set_draw_color( Color::RGB(0,0,0));
-        renderer.clear();
-        renderer.present();
+
+        before = now;
+        fps += 1;
+
+       if now - last_second > 1_000 {
+           println!("FPS: {}", fps);
+           last_second = now;
+           fps = 0;
+       }
+
+        context.events.pump();
+        match current_view.render( &mut context, elapsed ){
+            ViewAction::None => context.renderer.present(),
+            ViewAction::Quit => break
+        }
     }
 }
--- src/views/mod.rs
+++ src/views/mod.rs
@@ -4,15 +4,15 @@ use sdl2::pixels::Color;
 pub struct DefaultView;
 
 impl View for DefaultView{
-    fn render(&self, conext: &mut Phi, _:f64) -> ViewAction {
+    fn render(&mut self, context: &mut Phi, _:f64) -> ViewAction {
         let renderer = &mut context.renderer;
         let events   = &context.events;
-
+        
         if events.now.quit || events.now.key_escape == Some( true ){
             return ViewAction::Quit;
         }
 
-        renderer.set_draw_ccolor(Color::RGB(0,0,0));
+        renderer.set_draw_color(Color::RGB(0,0,0));
         renderer.clear();
 
         ViewAction::None
