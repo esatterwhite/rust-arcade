33efe6261aa0f2f7642fc99367f4cd359f37193c
Eric Satterwhite <esatterwhite@wi.rr.com>
2016-09-04 16:07:31 -0500

    moving the game loop into phi module

--- src/main.rs
+++ src/main.rs
@@ -3,60 +3,8 @@ extern crate sdl2;
 mod phi;
 mod views;
 
-use phi::{Events, Phi, View, ViewAction};
-
-
 fn main() {
-
-    // initialize SDL2
-    let sdl_context =  sdl2::init().unwrap();
-    let video = sdl_context.video().unwrap();
-    let mut timer = sdl_context.timer().unwrap();
-
-    // create a new window
-    let window = video.window("Arcade Shooter", 800, 600)
-            .position_centered().opengl()
-            .build().unwrap();
-
-    let mut context = Phi{
-        events: Events::new( sdl_context.event_pump().unwrap() ),
-        renderer: window.renderer().accelerated().build().unwrap(),
-    };
-
-    let mut current_view: Box<View> = Box::new(self::views::DefaultView);
-
-    // Frame timing
-
-    let interval = 1_000 / 60;
-    let mut before = timer.ticks();
-    let mut last_second = timer.ticks();
-    let mut fps = 0u16;
-
-
-    loop {
-        let now = timer.ticks();
-        let delta = now - before;
-        let elapsed = delta as f64 / 1_000.0;
-
-
-        if delta < interval {
-            timer.delay( interval - delta );
-                continue;
-        }
-
-        before = now;
-        fps += 1;
-
-       if now - last_second > 1_000 {
-           println!("FPS: {}", fps);
-           last_second = now;
-           fps = 0;
-       }
-
-        context.events.pump();
-        match current_view.render( &mut context, elapsed ){
-            ViewAction::None => context.renderer.present(),
-            ViewAction::Quit => break
-        }
-    }
+    self::phi::spawn("Arcade Shooter", |_|{
+        Box::new(self::views::DefaultView)
+    });
 }
--- src/phi/mod.rs
+++ src/phi/mod.rs
@@ -5,6 +5,64 @@ mod events;
 
 use sdl2::render::Renderer;
 
+pub fn spawn<F>(title: &str, init:F) where F: Fn( &mut Phi ) -> Box<View> {
+
+
+    // initialize SDL2
+    let sdl_context =  sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+    let mut timer = sdl_context.timer().unwrap();
+
+    // create a new window
+    let window = video.window(&title, 800, 600)
+            .position_centered().opengl()
+            .build().unwrap();
+
+    let mut context = Phi{
+        events: Events::new( sdl_context.event_pump().unwrap() ),
+        renderer: window.renderer().accelerated().build().unwrap(),
+    };
+
+    let mut current_view = init(&mut context);
+
+
+    // Frame timing
+
+    let interval = 1_000 / 60;
+    let mut before = timer.ticks();
+    let mut last_second = timer.ticks();
+    let mut fps = 0u16;
+
+
+    loop {
+        let now = timer.ticks();
+        let delta = now - before;
+        let elapsed = delta as f64 / 1_000.0;
+
+
+        if delta < interval {
+            timer.delay( interval - delta );
+                continue;
+        }
+
+        before = now;
+        fps += 1;
+
+       if now - last_second > 1_000 {
+           println!("FPS: {}", fps);
+           last_second = now;
+           fps = 0;
+       }
+
+        context.events.pump();
+        match current_view.render( &mut context, elapsed ){
+            ViewAction::None => context.renderer.present(),
+            ViewAction::Quit => break
+        }
+    }
+
+}
+
 struct_events! {
     keyboard: {
         key_escape: Escape,
