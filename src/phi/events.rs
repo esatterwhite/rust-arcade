e6ed87b0a5713675a59fb62cdfeca4931d173cb4
Eric Satterwhite <esatterwhite@wi.rr.com>
2016-09-04 23:38:30 -0500

    match on the window event to handler resizes

--- src/phi/events.rs
+++ src/phi/events.rs
@@ -52,9 +52,14 @@ macro_rules! struct_events {
 
                 for event in self.pump.poll_iter() {
                     use sdl2::event::Event::*;
+                    use sdl2::event::WindowEventId::Resized;
                     use sdl2::keyboard::Keycode::*;
 
                     match event {
+                        Window { win_event_id: Resized, .. } =>{
+                            self.now.resize = Some(renderer.output_size().unwrap());
+                        },
+                        
                         KeyDown { keycode, .. } => match keycode {
                             $(
                                 Some($k_sdl)=>{
