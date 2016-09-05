135f98fb8381c902e8d3767d6d33a4ed68bbaefd
Eric Satterwhite <esatterwhite@wi.rr.com>
2016-09-04 21:41:26 -0500

    add moveemnt to direction keys

--- src/views/ship.rs
+++ src/views/ship.rs
@@ -17,7 +17,7 @@ pub struct Rectangle {
 }
 
 impl Rectangle {
-    pub fn toSdl( self ) -> Option<SdlRect> {
+    pub fn to_sdl( self ) -> Option<SdlRect> {
         assert!(self.w >= 0.0 && self.h >= 0.0 );
         SdlRect::new( self.x as i32, self.y as i32, self.w as u32, self.h as u32 ).unwrap()
     }
@@ -31,7 +31,7 @@ impl ShipView {
     pub fn new(phi: &mut Phi) -> Self {
         ShipView {
             player: Ship {
-                rect: {
+                rect: Rectangle {
                     x: 64.0,
                     y: 64.0,
                     w: 32.0,
@@ -46,11 +46,35 @@ impl View for ShipView {
     fn render(&mut self, context: &mut Phi, elapsed:f64) -> ViewAction{
         let renderer = &mut context.renderer;
         let events   = &context.events;
-        let traveled = PLAYER_SPEED * elapsed
+        let traveled = PLAYER_SPEED * elapsed;
+
 
         if events.now.quit || events.now.key_escape == Some(true){
             return ViewAction::Quit
         }
+       
+        let diagonal = 
+            ( context.events.key_up ^ context.events.key_down) &&
+            ( context.events.key_left ^ context.events.key_right);
+
+        let moved =
+            if diagonal { 1.0 / 2.0f64.sqrt() }
+            else { 1.0 } * PLAYER_SPEED * elapsed;
+
+        let dy = match ( context.events.key_up, context.events.key_down ) {
+            ( true, true ) | ( false, false ) => 0.0,
+            ( true, false ) => -moved,
+            ( false, true ) => moved,
+        };
+
+        let dx = match ( context.events.key_left, context.events.key_right ) {
+            ( true, true ) | ( false, false ) => 0.0,
+            ( true, false ) => -moved,
+            ( false, true ) => moved,
+        };
+
+        self.player.rect.x += dx;
+        self.player.rect.y += dy;
        
         // view logic
         
